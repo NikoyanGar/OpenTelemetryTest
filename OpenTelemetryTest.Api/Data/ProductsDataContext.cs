19847bc714d91b1b1050d5d82359bec4c39b8516
nikoyangar <nikoyangar@gmail.com>
2024-01-23 20:38:42 +0400

    Add project files.

---
+++ OpenTelemetryTest.Api/Data/ProductsDataContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OpenTelemetryTest.Api.Data
+{
+
+    public class ProductsDataContext : DbContext
+    {
+        public ProductsDataContext()
+        {
+        }
+
+        public ProductsDataContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18, 2)");
+            modelBuilder.Entity<Product>()
+                .HasData(
+                    new Product
+                    {
+                        Id = 1,
+                        Name = "Mechanical Keyboard",
+                        Description = "RGB mechanical keyboard with Cherry MX Red switches",
+                        Price = 99.99M
+                    },
+                    new Product
+                    {
+                        Id = 2,
+                        Name = "Gaming Mouse",
+                        Description = "RGB gaming mouse with 7 programmable buttons",
+                        Price = 69.99M
+                    }
+                );
+        }
+
+    }
+}
---
+++ OpenTelemetryTest.Api/NeedToDO.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenTelemetryTest.Api
+{
+    public class NeedToDO
+    {
+        //docker pull mcr.microsoft.com/mssql/server
+
+        //docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=P@ssw0rd' -p 1433:1433 --name sql_server_container -d mcr.microsoft.com/mssql/server
+        //update-database
+
+        //docker run -d --name jaeger -p 16686:16686 -p 6831:6831/udp jaegertracing/all-in-one:1.21
+    }
+}
